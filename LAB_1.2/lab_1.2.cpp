308a4cb2e34beda27359618f0278514b3ebbb143
Bohdan <bohdan.kovba1812@gmail.com>
2023-09-27 02:46:28 +0300

    Перший коміт

---
+++ LAB_1.2/lab_1.2.cpp
@@ -0,0 +1,30 @@
+// Lab_02.cpp
+// < Ковба Богдан >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 9
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double a;
+	double b;
+	double z1;
+	double z2;
+
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+
+	z1 = (cos(a) - cos(b)) * (cos(a) - cos(b)) - (sin(a) - sin(b)) * (sin(a) - sin(b));
+	z2 = -4 * sin((a - b) / 2) * sin((a - b) / 2) * cos(a + b);
+
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
